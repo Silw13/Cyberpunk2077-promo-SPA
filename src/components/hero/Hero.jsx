c1f17a1f985a2ec1ea24880334c515d67911947e
Куницкий Ростислав Борисович <kunitskiy.rb@cso.com>
2023-11-13 16:54:33 +0300

    slider fixes

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,4 +1,5 @@
 import Hero_slider from "./Hero_slider";
+import './Hero.sass'
 
 export default function Hero() {
     return (
@@ -6,7 +7,7 @@ export default function Hero() {
             <Hero_slider />
             <div className="container">
                 <div className="hero__show-more-block">
-                    <h1 className="hero__h1"></h1>
+                    <h1 className="hero__h1">Доступно на всех платформах</h1>
                     <button className="hero__button">Узнать больше</button>
                 </div>
             </div>
